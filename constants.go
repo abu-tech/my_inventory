0fedffbe26bf780954a02412077575468c971ef3
abu-tech <abutech8801@gmail.com>
2024-06-25 04:21:41 +0530

    Stop tracking constants.go

--- constants.go
+++
@@ -1,7 +0,0 @@
-package main
-
-const DbUser = "postgres.svhyhjgdoyurzbbkmssm"
-const DbPassword = "gLqiFDW7yO6flnQg"
-const Dbname = "postgres"
-const Host = "aws-0-ap-south-1.pooler.supabase.com"
-const Port = "6543"
